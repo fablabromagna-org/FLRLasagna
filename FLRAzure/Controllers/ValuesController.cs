cd0a911314fdf26a03c574a3c2c19e4867c19703
Maurizio <maurizio@MacBook-Pro-di-Xamarin.local>
2018-12-28 17:00:19 +0100

    Primo commit

---
+++ FLRAzure/Controllers/ValuesController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FLRAzure.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LasagnaController : ControllerBase
+    {
+        static List<Lasagna> Lasagne = new List<Lasagna>();
+
+        // GET api/lasagna
+        [HttpGet]
+        public IEnumerable<Lasagna> Get()
+        {
+            return Lasagne;
+        }
+
+        // GET api/lasagna/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/lasagna
+        [HttpPost]
+        public void Pippo([FromBody] Lasagna value)
+        {
+            Lasagne.Add(value);
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+
+    public class Lasagna 
+    {
+        public string Nome{get;set;}
+        public string Peso{get;set;}
+        public string UrlImmagine{get;set;}
+    }
+}
---
+++ FLRLasagna/FLRLasagna/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Xamarin.Forms;
+
+namespace FLRLasagna
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void Go_Clicked(object sender, System.EventArgs e)
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+
+                    string response = await client.GetStringAsync(
+                                 "https://flr.azurewebsites.net/api/Lasagna");
+
+                    var result = JsonConvert.DeserializeObject
+                                 <IEnumerable<Lasagna>>(response);
+
+                    lvDati.ItemsSource = result;
+                }
+            }
+            catch (Exception err)
+            {
+                await DisplayAlert("Ocio!!", err.Message, "Ok");
+            }
+        }
+
+        void MaggioriInfo(object sender, System.EventArgs e)
+        {
+            DisplayAlert("Info", "Hai chiesto maggiori info...", "OK");
+        }
+
+        void Ordina(object sender, System.EventArgs e)
+        {
+            DisplayAlert("Compra", "Hai comprato una lasagna...", "OK");
+        }
+    }
+
+    public class Lasagna
+    {
+        public string Nome { get; set; }
+        public string Peso { get; set; }
+        public string UrlImmagine { get; set; }
+    }
+
+}
